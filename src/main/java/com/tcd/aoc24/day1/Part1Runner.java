276fe0633be834295fbd9a8aa4b590dab516ec71
demarcot <tom_demarco1013@yahoo.com>
2024-12-01 18:42:30 -0500

    feat(Tom): d1p1 d1p2

--- src/main/java/com/tcd/aoc24/day1/Part1Runner.java
+++ src/main/java/com/tcd/aoc24/day1/Part1Runner.java
@@ -3,7 +3,6 @@ package com.tcd.aoc24.day1;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.tcd.aoc24.utils.MiscUtils;
@@ -13,7 +12,35 @@ public class Part1Runner {
   public static void main(String[] args) throws URISyntaxException {
     System.out.println("--- Day 1 Part 1 ---");
     // Read file
-    // List<String> lines = MiscUtils.ReadFile("day1/d1p1.txt");
-
+    List<String> lines = MiscUtils.ReadFile("day1/d1p1.txt");
+    
+    String delim = "   ";
+    
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+    
+    for (String l : lines) {
+      String[] nums = l.split(delim);
+      
+      list1.add(Integer.parseInt(nums[0]));
+      list2.add(Integer.parseInt(nums[1]));
+    }
+    
+    Collections.sort(list1);
+    Collections.sort(list2);
+    
+    int totalDistance = 0;
+    
+    for (int i = 0; i < list1.size(); i++) {
+      int diff = list1.get(i) - list2.get(i);
+      
+      if (diff < 0) {
+        diff *= -1;
+      }
+      
+      totalDistance += diff;
+    }
+    
+    System.out.println("Total Distance: " + totalDistance);
   } 
 }
--- src/main/java/com/tcd/aoc24/day1/Part2Runner.java
+++ src/main/java/com/tcd/aoc24/day1/Part2Runner.java
@@ -3,7 +3,6 @@ package com.tcd.aoc24.day1;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.tcd.aoc24.utils.MiscUtils;
@@ -13,7 +12,41 @@ public class Part2Runner {
   public static void main(String[] args) throws URISyntaxException {
     System.out.println("--- Day 1 Part 2 ---");
     // Read file
-    // List<String> lines = MiscUtils.ReadFile("day1/d1p2.txt");
+    List<String> lines = MiscUtils.ReadFile("day1/d1p1.txt");
+    
+    String delim = "   ";
+    
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+    
+    for (String l : lines) {
+      String[] nums = l.split(delim);
+      
+      list1.add(Integer.parseInt(nums[0]));
+      list2.add(Integer.parseInt(nums[1]));
+    }
+    
+    Collections.sort(list1);
+    Collections.sort(list2);
+    
+    int similarityScore = 0;
+    
+    for (int i = 0; i < list1.size(); i++) {
+      int curr = list1.get(i);
+      int idx1 = list2.indexOf(curr);
+      
+      if (idx1 == -1) {
+        continue;
+      }
+      
+      int idx2 = list2.lastIndexOf(curr);
+      
+      int count = idx2 - idx1 + 1;
+      
+      similarityScore += (curr * count);
+    }
+    
+    System.out.println("Similarity Score: " + similarityScore);
 
   } 
 }
